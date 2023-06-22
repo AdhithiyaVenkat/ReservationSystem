97929dd184a895c7678717fddb66a2bc9b81d3e4
Adhithiya Venkatesh <adhithiyakannan@gmail.com>
2023-06-22 09:22:40 +0530

    Updated UI, backend database changed to MongoDB

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -1,63 +1,22 @@
 // Route.js
 const express = require("express")
+
+const { MongoClient } = require("mongodb");
+
 const router = express.Router();
-const fs = require('fs');
 
-const crypto = require("crypto");
+router.route("/").get((req, res) => {
+  const cl = new MongoClient("mongodb://localhost:27017");
+  async function run(){
+    await cl.connect();
+    const dbs = client.db("intro");
+    const coll = dbs.collection("quotes");
 
-const dataPath = './Details/userDetails.json' // path to our JSON file
-
-// util functions
-const saveAccountData = (data) => {
-    const stringifyData = JSON.stringify(data)
-    fs.writeFileSync(dataPath, stringifyData)
-}
-const getAccountData = () => {
-    const jsonData = fs.readFileSync(dataPath)
-    return JSON.parse(jsonData)   
-}
-
-router.post('/user/register', (req, res) => {
-
-  console.table(req.body);
- 
-    var existAccounts = getAccountData()
-    
-    const newAccountId = crypto.randomBytes(16).toString("hex");
- 
-    existAccounts[newAccountId] = req.body
-   
-    console.log(existAccounts);
-    saveAccountData(existAccounts);
-    res.send({success: true, id: newAccountId})
-})
-
-// Read - get all accounts from the json file
-router.get('/user/list', (req, res) => {
-    const accounts = getAccountData()
-    res.send(accounts)
-})
-
-// Update - using Put method
-router.put('/user/:id', (req, res) => {
-    var existAccounts = getAccountData()
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-      const accountId = req.params['id'];
-      existAccounts[accountId] = req.body;
-      saveAccountData(existAccounts);
-      res.send(`accounts with id ${accountId} has been updated`)
-    }, true);
-  });
-
-  // delete - using delete method
-router.delete('/user/:id', (req, res) => {
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-      var existAccounts = getAccountData()
-      const userId = req.params['id'];
-      delete existAccounts[userId]; 
-      saveAccountData(existAccounts);
-      res.send(`accounts with id ${userId} has been deleted`)
-    }, true);
-  })
+    const rest = await coll.insertOne({"quote":"This is my quote."});
+    res.end(JSON.stringify(rest));
+  }
+  run();
+});
+// router.route("/:id").get(getBlogById).put(updateBlog).delete(deleteBlog);
 
 module.exports = router;
